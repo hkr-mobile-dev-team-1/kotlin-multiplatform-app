a5075f61d9a53f0b54697daedcfb2ea404735af5
André Sandblom <159018987+AndreSandblom@users.noreply.github.com>
2025-10-30 10:36:27 +0100

    Updated User, Team and Teamembers Repository (#77)
    * added dataclasses from the db tables and some intial repositories to be used aswell
    
    * added and corrected repositories
    
    * Delete composeApp/src/commonMain/kotlin/com/teamschedulerapp/model/Task2.kt
    
    deleted task2 that somehow still managed to sneak in.

--- composeApp/src/commonMain/kotlin/com/teamschedulerapp/repositories/TeamMemberRepository.kt
+++ composeApp/src/commonMain/kotlin/com/teamschedulerapp/repositories/TeamMemberRepository.kt
@@ -1,17 +1,26 @@
 package com.teamschedulerapp.repositories
 
 import com.teamschedulerapp.model.TeamMember
+import com.teamschedulerapp.model.User
 import io.github.jan.supabase.postgrest.Postgrest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 
-class TeamMemberRepository(private val postgrest: Postgrest) {
+class TeamMemberRepository(
+    private val postgrest: Postgrest,
+    private val userRepository: UserRepository
+) {
 
-    suspend fun addMemberToTeam(member: TeamMember): Boolean {
+    suspend fun addMemberToTeam( teamId: String, userId: String, isAdmin: Boolean = false): Boolean {
         return try {
             withContext(Dispatchers.IO) {
-                postgrest.from("team_members").insert(member)
+                postgrest.from("team_members").insert(
+                    mapOf(
+                        "team_id" to teamId,
+                        "user_id" to userId,
+                        "is_admin" to isAdmin
+                    ))
             }
             true
         } catch (e: Exception) {
@@ -20,17 +29,35 @@ class TeamMemberRepository(private val postgrest: Postgrest) {
         }
     }
 
+    // Idea will be that the invitation link will parse the token with encoded teamId
+    // and the invited users id to then be calling the function below
+    suspend fun acceptTeamInvitation(teamId: String, userId: String): Boolean{
+        return addMemberToTeam(teamId,userId)
+    }
     suspend fun getMembersForTeam(teamId: String): List<TeamMember> {
         return try {
             withContext(Dispatchers.IO) {
-                postgrest.from("team_members").select {
-                    filter { eq("team_id", teamId) }
+                postgrest.from("team_members")
+                    .select {
+                        filter { eq("team_id", teamId) }
                 }.decodeList<TeamMember>()
             }
         } catch (e: Exception) {
             println("Error fetching members: ${e.message}")
             emptyList()
         }
+    }
+
+    suspend fun getTeamWithUserInfo(teamId: String): List<User> {
+        val teamMembers = getMembersForTeam(teamId)
+        val users = mutableListOf<User>()
+
+        for(member in teamMembers) {
+            val user = userRepository.getUserById(member.userId)
+            user?.let { users.add(it) }
+        }
+
+        return users
     }
 
     suspend fun setMemberAdmin(teamId: String, userId: String, isAdmin: Boolean): Boolean {
@@ -68,6 +95,4 @@ class TeamMemberRepository(private val postgrest: Postgrest) {
             false
         }
     }
-
-
 }
