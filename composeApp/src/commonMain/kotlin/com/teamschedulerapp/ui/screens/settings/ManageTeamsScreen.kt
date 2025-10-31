eb9132464b16ae12c923c9c2cff61697a8cb785a
Di7-prog <dimple.narkhede0004@stud.hkr.se>
2025-10-31 11:13:40 +0100

    Update composeApp/src/commonMain/kotlin/com/teamschedulerapp/ui/screens/settings/ManageTeamsScreen.kt
    Co-authored-by: Elina Rosato <rosatoelina@gmail.com>

--- composeApp/src/commonMain/kotlin/com/teamschedulerapp/ui/screens/settings/ManageTeamsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/teamschedulerapp/ui/screens/settings/ManageTeamsScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import com.teamschedulerapp.model.TeamWithMembers
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ManageTeamsScreen(
     userId: String,
