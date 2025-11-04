f81a0d186fc09171be74a809a2b36446ed94b42f
Dario Ostojic <darioostojic2@gmail.com>
2025-11-04 23:28:36 +0100

    Deleted an unused import in App.kt

--- composeApp/src/commonMain/kotlin/com/teamschedulerapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/teamschedulerapp/App.kt
@@ -1,6 +1,5 @@
 package com.teamschedulerapp
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
 import com.teamschedulerapp.navigation.Login
